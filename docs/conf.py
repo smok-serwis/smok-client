c18d6973fb630df389c83b264769ec0172988984
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-12-31 01:56:19 +0100

    fix licensing

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'SMOK Client'
-copyright = '2020, Piotr Maślanka'
+copyright = '2010-2020, SMOK sp. z o. o.'
 author = 'Piotr Maślanka'
 
 # The full version, including alpha/beta/rc tags
