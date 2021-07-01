5f47aaa2fa617abb339c495f44936ba750536dd5
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-07-01 18:00:34 +0200

    thrown support for python 3.5 outta airlock

--- setup.py
+++ setup.py
@@ -6,5 +6,4 @@ setup(version=__version__,
       packages=find_packages(include=['smok', 'smok.*', 'ngtt', 'ngtt.*']),
       install_requires=[line.strip() for line in open('requirements.txt', 'r').readlines() if
                         line.strip()],
-      python_requires='!=2.7.*,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*'
       )
